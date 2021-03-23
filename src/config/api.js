14343cbb031934e2bd99168d0ac61c8bb51a3ef2
mahmoud-batman <mahmoud148043@gmail.com>
2021-03-23 15:09:22 +0200

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,21 @@
+export const API = 'https://motoolstore.com/';
+
+export const CONSUMER_KEY = 'ck_abc2533c1787a1c91bd07ea340455ebbe54a8be6';
+export const CONSUMER_SECRET = 'cs_ed2a7b904b81a76810c2658c9815489250f35c17';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
+
+// export const API = 'https://wc2.rnlab.io';
+
+// export const CONSUMER_KEY = 'ck_9a40d82e47c124bc1be422682dd93eb25bdb5e3a';
+// export const CONSUMER_SECRET = 'cs_01bd2047d4ea33bdb4b6d8f6f034e0e072abdf4a';
+
+// export default {
+//   API_ENDPOINT: API,
+//   CONSUMER_KEY,
+//   CONSUMER_SECRET,
+// };
